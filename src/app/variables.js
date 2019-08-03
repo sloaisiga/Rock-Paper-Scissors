da57274d3da1b79a7f483270f5db5e64c97e0e40
Scarleth <sloaisiga18@gmail.com>
2019-08-03 12:13:54 -0600

    add userWins and alexaWins variables

--- src/app/variables.js
+++ src/app/variables.js
@@ -1,3 +1,7 @@
-exports.name = function(voxaEvent) {
-  return voxaEvent.model.name;
+exports.userWins = function(voxaEvent) {
+  return voxaEvent.model.userWins;
+};
+
+exports.alexaWins = function(voxaEvent) {
+  return voxaEvent.model.alexaWins;
 };
