bda442f00728091388737d32167646fe6b91ba9a
Scarleth <sloaisiga18@gmail.com>
2019-08-03 12:20:35 -0600

    add CancelIntent and StopIntent intents

--- src/app/states/main.states.js
+++ src/app/states/main.states.js
@@ -163,6 +163,20 @@ function register(voxaApp) {
     }
   });
 
+  voxaApp.onIntent("CancelIntent", () => {
+    return {
+      flow: "terminate",
+      reply: "Bye",
+    };
+  });
+
+  voxaApp.onIntent("StopIntent", () => {
+    return {
+      flow: "terminate",
+      reply: "Bye",
+    };
+  });
+
   //jhilnk
 }
 
