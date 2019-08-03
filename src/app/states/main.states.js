3184ee5d94925de69997c0614f30adf959874a80
Scarleth <sloaisiga18@gmail.com>
2019-08-03 11:46:16 -0600

    add getHowManyWins state

--- src/app/states/main.states.js
+++ src/app/states/main.states.js
@@ -14,6 +14,19 @@ function register(voxaApp) {
       to: "getHowManyWins",
     };
   });
+
+  voxaApp.onState("getHowManyWins", voxaEvent => {
+    if (voxaEvent.intent.name === "MaxWinsIntent") {
+      voxaEvent.model.wins = voxaEvent.intent.params.wins;
+      voxaEvent.model.userWins = 0;
+      voxaEvent.model.alexaWins = 0;
+      return {
+        flow: "continue",
+        reply: "StartGame",
+        to: "askUserChoice",
+      };
+    }
+  });
 }
 
 module.exports = register;
