f839c58d60d4e51ced537dd78733b91b406bc047
Scarleth <sloaisiga18@gmail.com>
2019-08-03 11:57:58 -0600

    add askUserChoice and getUserChoice states

--- src/app/states/main.states.js
+++ src/app/states/main.states.js
@@ -27,6 +27,60 @@ function register(voxaApp) {
       };
     }
   });
+
+  const CHOICES = ["rock", "paper", "scissors"];
+
+  voxaApp.onState("askUserChoice", voxaEvent => {
+    const userWon =
+      parseInt(voxaEvent.model.userWins) >= parseInt(voxaEvent.model.wins);
+    const alexaWon =
+      parseInt(voxaEvent.model.alexaWins) >= parseInt(voxaEvent.model.wins);
+
+    if (userWon) {
+      return {
+        flow: "continue",
+        reply: "UserWinsTheGame",
+        to: "askIfStartANewGame",
+      };
+    }
+
+    if (alexaWon) {
+      return {
+        flow: "continue",
+        reply: "AlexaWinsTheGame",
+        to: "askIfStartANewGame",
+      };
+    }
+
+    const min = 0;
+    const max = CHOICES.length - 1;
+    voxaEvent.model.userChoice = undefined;
+    voxaEvent.model.alexaChoice =
+      Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return {
+      flow: "yield",
+      reply: "AskUserChoice",
+      to: "getUserChoice",
+    };
+  });
+
+  voxaApp.onState("getUserChoice", voxaEvent => {
+    if (voxaEvent.intent.name === "RockIntent") {
+      voxaEvent.model.userChoice = "rock";
+    } else if (voxaEvent.intent.name === "PaperIntent") {
+      voxaEvent.model.userChoice = "paper";
+    } else if (voxaEvent.intent.name === "ScissorsIntent") {
+      voxaEvent.model.userChoice = "scissors";
+    }
+
+    if (voxaEvent.model.userChoice) {
+      return {
+        flow: "continue",
+        to: "processWinner",
+      };
+    }
+  });
 }
 
 module.exports = register;
